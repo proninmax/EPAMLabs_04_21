a70ca3477a54764864cb5551e6a8b4b8ddc533be
proninmax <71521863+proninmax@users.noreply.github.com>
2021-04-21 22:27:42 +0300

    Lab1_v0.1

---
+++ Lab1/Maksim_Pronin_Epam_Task1/Maksim_Pronin_Epam_Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Maksim_Pronin_Epam_Task1
+{
+    class Program
+    {
+        static void SortedArr(double[] arr)
+        {
+
+            for (int j = 0; j < arr.Length; j++) 
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if(arr[i] > arr[i + 1])
+                    {
+                        double lokalSortArr = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = lokalSortArr;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Arr[" + (i + 1) + "] = " + arr[i]);
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the size of the array");
+            int syzeArr = int.Parse(Console.ReadLine());
+            double[] array = new double[syzeArr];
+            for (int i = 0; i < syzeArr; i++)
+            {
+                Console.Write("Arr[" + (i + 1) + "] = ");
+                array[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+            SortedArr(array);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab1/Maksim_Pronin_Epam_Task1/Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char operation;
+            bool stopedGob = true;
+            double arg1;
+            double arg2;
+            double result;
+            while(stopedGob)
+            {
+                Console.WriteLine("Enter first arg");
+                arg1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter operation(-,+,*,/)");
+                operation = Char.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second arg");
+                arg2 = double.Parse(Console.ReadLine());
+                switch (operation)
+                {
+                    case  '+':
+                        result = arg1 + arg2;
+                        break;
+                    case '-':
+                        result = arg1 - arg2;
+                        break;
+                    case '/':
+                        result = arg1 / arg2;
+                        break;
+                    case '*':
+                        result = arg1 * arg2;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation");
+                        result = double.MinValue;
+                        break;
+                }
+                Console.WriteLine(arg1 + operation.ToString() + arg2 + "=" + result);
+                Console.WriteLine("Click 'y' if you want to continue");
+                if (Console.ReadLine() != "y")
+                {
+                    stopedGob = false;
+                }
+            }
+        }
+    }
+}
