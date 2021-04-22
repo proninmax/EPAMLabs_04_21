ad1e7a5b2eec54b7614f3920501e2a4bfd854330
proninmax <71521863+proninmax@users.noreply.github.com>
2021-04-22 22:55:11 +0300

    Lab2 V0.1

---
+++ Lab2_Pronin_Maksim/Lab2_Pronin_Maksim/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab2_Pronin_Maksim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Ferrary", 300, 700);
+            car.GetInfo();
+            car.Drive(10, 23);
+            Console.ReadKey();
+            Plane plane = new Plane("Mig-35", 2500, 3000, 10);
+            plane.GetInfo();
+            plane.Fly_up(100, 100, 0.03);
+            Console.ReadLine();
+        }
+    }
+}
